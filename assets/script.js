f941aa47c99c7cc876ef6bc6b6074f79919cb5a3
peterhohk <peterhohk0409@gmail.com>
2024-06-23 19:54:03 +0800

    rename assets

--- assets/script.js
+++ assets/script.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
             <div class="artwork-feature-info">
               <div class="artwork-feature-text-container"><span class="artwork-feature-text">Featured</span></div>
               <div class="artwork-feature-badge-glow"></div>
-              <img src="./assets/img/feature_badge.svg" alt="Featured artwork badge" class="artwork-feature-badge">
+              <img src="./assets/img/feature-badge.svg" alt="Featured artwork badge" class="artwork-feature-badge">
             </div>
           ` : ``}
         </button>
